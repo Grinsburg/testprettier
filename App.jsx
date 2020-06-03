f91214bd9916ceb1fe77230c7ec110f39ad1ae31
Grinsburg <smlynchik@mail.ru>
2020-06-03 12:16:11 +0300

    fdsf

--- App.jsx
+++ App.jsx
@@ -12,8 +12,8 @@ import Login from './pages/Login/Login';
 import routes from './modules/navigation/routes';
 import history from './modules/navigation/history';
 import {
-	authorizationUser,
-	authorizationIsLoading,
+  authorizationUser,
+  authorizationIsLoading,
 } from './modules/redux/authorization/authorization.selectors';
 import { Layout } from 'antd';
 
@@ -21,61 +21,61 @@ import 'antd/dist/antd.css';
 import './theme/global.scss';
 
 export class App extends React.Component {
-	render() {
-		return (
-			<Router history={history}>
-				{this.props.user === null ? (
-					<Login />
-				) : (
-					<Layout>
-						<Layout.Sider>
-							<SideBar />
-						</Layout.Sider>
-						<Layout>
-							<Layout.Content>
-								<Switch>
-									<Route
-										exact
-										path={routes.recommendations}
-										component={AllRecommendations}
-									/>
-									<Route
-										exact
-										path={routes.newRecommendation}
-										component={NewRecommendation}
-									/>
-									<Route exact path={routes.about} component={About} />
-									<Route
-										exact
-										path={routes.recommendationId}
-										component={Recommentaion}
-									/>
-								</Switch>
-							</Layout.Content>
-						</Layout>
-					</Layout>
-				)}
-			</Router>
-		);
-	}
+  render() {
+    return (
+      <Router history={history}>
+        {this.props.user === null ? (
+          <Login />
+        ) : (
+          <Layout>
+            <Layout.Sider>
+              <SideBar />
+            </Layout.Sider>
+            <Layout>
+              <Layout.Content>
+                <Switch>
+                  <Route
+                    exact
+                    path={routes.recommendations}
+                    component={AllRecommendations}
+                  />
+                  <Route
+                    exact
+                    path={routes.newRecommendation}
+                    component={NewRecommendation}
+                  />
+                  <Route exact path={routes.about} component={About} />
+                  <Route
+                    exact
+                    path={routes.recommendationId}
+                    component={Recommentaion}
+                  />
+                </Switch>
+              </Layout.Content>
+            </Layout>
+          </Layout>
+        )}
+      </Router>
+    );
+  }
 }
 
 Route.propTypes = {
-	path: PropTypes.string,
-	exact: PropTypes.bool,
-	component: PropTypes.oneOfType([PropTypes.object, PropTypes.func]),
-	render: PropTypes.func,
+  path: PropTypes.string,
+  exact: PropTypes.bool,
+  component: PropTypes.oneOfType([PropTypes.object, PropTypes.func]),
+  render: PropTypes.func,
 };
 
 App.propTypes = {
-	user: PropTypes.object,
+  user: PropTypes.object,
 };
 
 const mapStateToProps = (state) => {
-	return {
-		isLoading: authorizationIsLoading(state),
-		user: authorizationUser(state),
-	};
+  return {
+    isLoading: authorizationIsLoading(state),
+    user: authorizationUser(state),
+  };
 };
 
 export default connect(mapStateToProps)(App);
--- RecommendationForm.jsx
+++ RecommendationForm.jsx
@@ -11,8 +11,8 @@ import routes from '../../modules/navigation/routes';
 import { LOCATIONS } from './RecommdendationForm.constants';
 import { submitRecommendationForm } from '../../modules/redux/recommendationForm/recommendationForm.actions';
 import {
-	submitRecomendationFormIsLoading,
-	getAllDivisions,
+  submitRecomendationFormIsLoading,
+  getAllDivisions,
 } from '../../modules/redux/recommendationForm/recommendationForm.selectors';
 import { createRecommendationSchema, initialValues } from '../../schemas/';
 
@@ -20,226 +20,226 @@ import styles from './RecommendationForm.module.scss';
 import { QuestionCircleOutlined } from '@ant-design/icons';
 
 export class RecommendationForm extends React.Component {
-	render() {
-		const {
-			isLoading,
-			divisions,
-			submitRecommendationForm,
-			history,
-		} = this.props;
-		const { TextArea } = Input;
+  render() {
+    const {
+      isLoading,
+      divisions,
+      submitRecommendationForm,
+      history,
+    } = this.props;
+    const { TextArea } = Input;
 
-		if (isLoading) {
-			return <Spin size='large' />;
-		}
+    if (isLoading) {
+      return <Spin size='large' />;
+    }
 
-		return (
-			<Formik
-				onSubmit={(value) => {
-					const contacts = {
-						firstName: value.firstName,
-						lastName: value.lastName,
-						feedback: value.feedback,
-						divisionId: value.division.value,
-						location: value.location.value,
-						linkedin: value.LinkedIn,
-						email: value.Email,
-						phone: value.Phone,
-					};
-					return submitRecommendationForm(contacts).then(() => {
-						history.push(routes.recommendations);
-					});
-				}}
-				initialValues={initialValues}
-				validationSchema={createRecommendationSchema}
-			>
-				{({ errors, touched }) => (
-					<Form layout='vertical' size='large' style={{ width: '100%' }}>
-						<Row justify='center'>
-							<Col span={18}>
-								<Row>
-									<Col span={24} style={{ marginTop: '3rem' }}>
-										<Row justify='space-between'>
-											<Col span={10}>
-												<Form.Item
-													label='First Name'
-													name='firstName'
-													hasFeedback
-													rules={[
-														{
-															required: true,
-															message: 'Please input your first name!',
-														},
-													]}
-												>
-													<Input placeholder='First Name' />
-												</Form.Item>
-												<Form.Item
-													label='Last Name'
-													name='lastName'
-													hasFeedback
-													rules={[
-														{
-															required: true,
-															message: 'Please input your last name!',
-														},
-													]}
-												>
-													<Input placeholder='Last Name' />
-												</Form.Item>
-												<Form.Item
-													label='Division'
-													name='division'
-													hasFeedback
-													rules={[
-														{
-															required: true,
-															message: 'Please choose the division!',
-														},
-													]}
-												>
-													<Select
-														placeholder='Choose the division'
-														options={divisions}
-													></Select>
-												</Form.Item>
-												<Form.Item
-													label='Location'
-													name='location'
-													hasFeedback
-													rules={[
-														{
-															required: true,
-															message: 'Please choose the location!',
-														},
-													]}
-												>
-													<Select
-														placeholder='Choose the location'
-														options={LOCATIONS}
-													></Select>
-												</Form.Item>
-											</Col>
-											<Col span={10}>
-												<Form.Item
-													label='Email'
-													name='email'
-													rules={[
-														{
-															required: false,
-															type: 'email',
-															message: 'The input is not valid E-mail!',
-														},
-													]}
-												>
-													<Input placeholder='Email' />
-												</Form.Item>
-												<Form.Item
-													label='Linkedin'
-													name='linkedin'
-													rules={[
-														{
-															required: false,
-															message: 'Please enter valid Linkedin!',
-														},
-													]}
-												>
-													<Input placeholder='Linkedin' />
-												</Form.Item>
-												<Form.Item
-													label={
-														<span>
-															Phone&nbsp;
-															<Tooltip title='Example: 375295555555'>
-																<QuestionCircleOutlined />
-															</Tooltip>
-														</span>
-													}
-													name='phone'
-													rules={[
-														{
-															max: 12,
-															message: `Max length 12 letters`,
-														},
-														{
-															min: 12,
-															message: `Min length 12 letters`,
-														},
-													]}
-												>
-													<Input placeholder='Phone' />
-												</Form.Item>
-											</Col>
-										</Row>
-									</Col>
-									<Col span={24}>
-										<Form.Item
-											label='Feedback'
-											name='feedback'
-											rules={[
-												{
-													required: true,
-													message: 'Please enter your feedback',
-												},
-											]}
-										>
-											<TextArea autoSize={{ minRows: 12, maxRows: 12 }} />
-										</Form.Item>
-									</Col>
-									<Col span={24}>
-										<Row justify='space-between'>
-											<Col span={3}>
-												<Form.Item>
-													<Button
-														data-test='cancel'
-														onClick={() => {
-															history.push(routes.recommendations);
-														}}
-														type='primary'
-														className={classNames(styles.button)}
-													>
-														Cancel
-													</Button>
-												</Form.Item>
-											</Col>
-											<Col span={3}>
-												<Form.Item>
-													<Button
-														data-test='submit'
-														type='primary'
-														htmlType='submit'
-														className={classNames(styles.button)}
-													>
-														Submit
-													</Button>
-												</Form.Item>
-											</Col>
-										</Row>
-									</Col>
-								</Row>
-							</Col>
-						</Row>
-					</Form>
-				)}
-			</Formik>
-		);
-	}
+    return (
+      <Formik
+        onSubmit={(value) => {
+          const contacts = {
+            firstName: value.firstName,
+            lastName: value.lastName,
+            feedback: value.feedback,
+            divisionId: value.division.value,
+            location: value.location.value,
+            linkedin: value.LinkedIn,
+            email: value.Email,
+            phone: value.Phone,
+          };
+          return submitRecommendationForm(contacts).then(() => {
+            history.push(routes.recommendations);
+          });
+        }}
+        initialValues={initialValues}
+        validationSchema={createRecommendationSchema}
+      >
+        {({ errors, touched }) => (
+          <Form layout='vertical' size='large' style={{ width: '100%' }}>
+            <Row justify='center'>
+              <Col span={18}>
+                <Row>
+                  <Col span={24} style={{ marginTop: '3rem' }}>
+                    <Row justify='space-between'>
+                      <Col span={10}>
+                        <Form.Item
+                          label='First Name'
+                          name='firstName'
+                          hasFeedback
+                          rules={[
+                            {
+                              required: true,
+                              message: 'Please input your first name!',
+                            },
+                          ]}
+                        >
+                          <Input placeholder='First Name' />
+                        </Form.Item>
+                        <Form.Item
+                          label='Last Name'
+                          name='lastName'
+                          hasFeedback
+                          rules={[
+                            {
+                              required: true,
+                              message: 'Please input your last name!',
+                            },
+                          ]}
+                        >
+                          <Input placeholder='Last Name' />
+                        </Form.Item>
+                        <Form.Item
+                          label='Division'
+                          name='division'
+                          hasFeedback
+                          rules={[
+                            {
+                              required: true,
+                              message: 'Please choose the division!',
+                            },
+                          ]}
+                        >
+                          <Select
+                            placeholder='Choose the division'
+                            options={divisions}
+                          ></Select>
+                        </Form.Item>
+                        <Form.Item
+                          label='Location'
+                          name='location'
+                          hasFeedback
+                          rules={[
+                            {
+                              required: true,
+                              message: 'Please choose the location!',
+                            },
+                          ]}
+                        >
+                          <Select
+                            placeholder='Choose the location'
+                            options={LOCATIONS}
+                          ></Select>
+                        </Form.Item>
+                      </Col>
+                      <Col span={10}>
+                        <Form.Item
+                          label='Email'
+                          name='email'
+                          rules={[
+                            {
+                              required: false,
+                              type: 'email',
+                              message: 'The input is not valid E-mail!',
+                            },
+                          ]}
+                        >
+                          <Input placeholder='Email' />
+                        </Form.Item>
+                        <Form.Item
+                          label='Linkedin'
+                          name='linkedin'
+                          rules={[
+                            {
+                              required: false,
+                              message: 'Please enter valid Linkedin!',
+                            },
+                          ]}
+                        >
+                          <Input placeholder='Linkedin' />
+                        </Form.Item>
+                        <Form.Item
+                          label={
+                            <span>
+                              Phone&nbsp;
+                              <Tooltip title='Example: 375295555555'>
+                                <QuestionCircleOutlined />
+                              </Tooltip>
+                            </span>
+                          }
+                          name='phone'
+                          rules={[
+                            {
+                              max: 12,
+                              message: `Max length 12 letters`,
+                            },
+                            {
+                              min: 12,
+                              message: `Min length 12 letters`,
+                            },
+                          ]}
+                        >
+                          <Input placeholder='Phone' />
+                        </Form.Item>
+                      </Col>
+                    </Row>
+                  </Col>
+                  <Col span={24}>
+                    <Form.Item
+                      label='Feedback'
+                      name='feedback'
+                      rules={[
+                        {
+                          required: true,
+                          message: 'Please enter your feedback',
+                        },
+                      ]}
+                    >
+                      <TextArea autoSize={{ minRows: 12, maxRows: 12 }} />
+                    </Form.Item>
+                  </Col>
+                  <Col span={24}>
+                    <Row justify='space-between'>
+                      <Col span={3}>
+                        <Form.Item>
+                          <Button
+                            data-test='cancel'
+                            onClick={() => {
+                              history.push(routes.recommendations);
+                            }}
+                            type='primary'
+                            className={classNames(styles.button)}
+                          >
+                            Cancel
+                          </Button>
+                        </Form.Item>
+                      </Col>
+                      <Col span={3}>
+                        <Form.Item>
+                          <Button
+                            data-test='submit'
+                            type='primary'
+                            htmlType='submit'
+                            className={classNames(styles.button)}
+                          >
+                            Submit
+                          </Button>
+                        </Form.Item>
+                      </Col>
+                    </Row>
+                  </Col>
+                </Row>
+              </Col>
+            </Row>
+          </Form>
+        )}
+      </Formik>
+    );
+  }
 }
 
 const mapStateToProps = (state) => {
-	return {
-		isLoading: submitRecomendationFormIsLoading(state),
-		divisions: getAllDivisions(state),
-	};
+  return {
+    isLoading: submitRecomendationFormIsLoading(state),
+    divisions: getAllDivisions(state),
+  };
 };
 
 RecommendationForm.propTypes = {
-	history: PropTypes.object,
-	isLoading: PropTypes.bool,
-	submitRecommendationForm: PropTypes.func,
-	divisions: PropTypes.array,
+  history: PropTypes.object,
+  isLoading: PropTypes.bool,
+  submitRecommendationForm: PropTypes.func,
+  divisions: PropTypes.array,
 };
 
 export default withRouter(
-	connect(mapStateToProps, { submitRecommendationForm })(RecommendationForm)
+  connect(mapStateToProps, { submitRecommendationForm })(RecommendationForm)
 );
